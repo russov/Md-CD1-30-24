50d4310571f749fcf646b478f105823d386cbf53
waldemurr <66300725+waldemurr@users.noreply.github.com>
2024-03-30 13:25:59 +0300

    bacward num read

--- Vladimir_Shedzko/Lesson 3/task_3_1.cpp
+++ Vladimir_Shedzko/Lesson 3/task_3_1.cpp
@@ -4,30 +4,52 @@
 
 #define FILE_NAME "numbers.txt"
 
-using namespace std;
-
 void readBackwards(){
-    ifstream inNums(FILE_NAME);
+    std::ifstream inNums(FILE_NAME, std::ios::ate);
+    std::string num;
+    int count = 0;
+    char curchar;
+    int pos = inNums.tellg();
+    inNums.seekg(-1, std::ios::end);
+    for (int i=0; i < pos; i++){
+        curchar = inNums.get();
+        count -= 1;
+        std::cout << inNums.cur << std::endl;
+        std::cout << curchar << std::endl;
+        if (curchar != '\n'){
+            num.insert (0, 1, curchar);
+            continue;
+        }
+        inNums.seekg(-2, std::ios::cur);
+        
+        
+        std::cout << num << std::endl;
+        num.clear();
+
+        // num = inNums + 
+    }
     inNums.close();
 }
 
 void getSum(){
     int sum = 0;
-    ifstream inNums(FILE_NAME);
+    std::ifstream inNums(FILE_NAME, std::ios::beg);
     int a;
     while (inNums >> a)
         sum += a;
     inNums.close();
-    cout << "Сумма чисел в файле: " << sum << endl;
+    std::cout << "Sum of numbers in file: " << sum << std::endl;
 }
 
 int main(){
-    ofstream outNums(FILE_NAME); 
-    cout << "Введите целые числа" << endl;
+    std::ofstream outNums(FILE_NAME, std::ios::app); 
+    std::cout << "Enter numbers here" << std::endl;
     int num;
-    while (cin >> num){
-        outNums << num;
+    while (std::cin){
+        std::cin >> num;
+        outNums << num << std::endl;
     }
     outNums.close();
     getSum();
+    readBackwards();
 }

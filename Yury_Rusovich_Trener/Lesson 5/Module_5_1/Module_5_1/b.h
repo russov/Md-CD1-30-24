cef884a4c82ea9d12962cc877280a9c9e5e6ea18
Jora <russov@tut.by>
2024-04-03 16:55:49 +0200

    add lesson 5

---
+++ Yury_Rusovich_Trener/Lesson 5/Module_5_1/Module_5_1/b.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+// Функция doOperation() выполняет операцию вычитания своих параметров
+int doOperation(int a, int b)
+{
+    return a - b;
+}
+
+
+//namespace B
+//{
+//    // Эта версия doOperation() принадлежит пространству имен Doo
+//    int doOperation(int a, int b)
+//    {
+//        return a - b;
+//    }
+//}

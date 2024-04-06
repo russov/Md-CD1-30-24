459deec86e294fcb392d0103257f321d9cdd5e2d
ms-mary <masha_makhnach@mail.ru>
2024-04-06 15:20:37 +0300

    task3

---
+++ Mariya_Mahnach/task3/task3 2/task3 2/task3 2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int CharNum(std::string s, char c)
+{
+    static bool gIscom = false;
+    int n = s.size(), k = 0;
+    bool iscom = false;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s[i] == '/' && s[i + 1] == '/' && !gIscom) { iscom = true; break; }
+        if (s[i] == '/' && s[i + 1] == '*' && !iscom) { gIscom = true; }
+        if (s[i] == '*' && s[i + 1] == '/' && !iscom) { gIscom = false; }
+        if (!iscom && !gIscom && s[i] == c) k++;
+    }
+    return k;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int open = 0, close = 0,k=0;
+    std::ifstream infile("task2.cpp");
+    if (!infile.is_open()) { std::cout << "Не удалось открыть файл ввода"; return 0; }
+    while (!infile.eof())
+    {
+        k++;
+        std::string str;
+        getline(infile, str, '\n');
+        if(k==1) str.erase(0,3);
+        //std::cout<<str<<"\n";
+        open += CharNum(str, '{');
+        close += CharNum(str, '}');
+    }
+    infile.close();
+    std::ofstream outfile("out.txt");
+    if (!outfile.is_open()) { std::cout << "Не удалось открыть файл вывода"; return 0; }
+    if (open == close)
+    {
+        std::cout << "Со скобками всё в порядке";
+        outfile << "Со скобками всё в порядке";
+    }
+    else if (open > close)
+    {
+        std::cout << "Не хватает '}' или есть лишние '{'";
+        outfile << "Не хватает '}' или есть лишние '{'";
+    }
+    else
+    {
+        std::cout << "Не хватает '{' или есть лишние '}'";
+        outfile << "Не хватает '{' или есть лишние '}'";
+    }
+    outfile.close();
+}
---
+++ Mariya_Mahnach/task3/task3/task3/task3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int n=10;
+    int a,b,arr[10][10];
+    std::cout<<"Введите интервал для генерации чисел массива:\n";
+    std::cin>>a>>b;
+    std::cout<<"Сгенерированный массив:\n";
+    if(a>b) std::swap(a,b);
+    int numi = 0, numj = 0;
+    std::srand(time(0));
+    for(int i=0;i<n;i++)
+    {
+        std::cout<<i+1<<")\t";
+        for(int j=0;j<n;j++)
+        {
+            arr[i][j]=std::rand()%(b-a)+a;
+            std::cout<<arr[i][j]<<'\t';
+            if(arr[i][j]<arr[numi][numj]) {numi=i;numj=j;}
+        }
+        std::cout<<'\n';
+    }
+    std::cout<<"\nМинимальный элемент: "<<arr[numi][numj]<<"\nКоординаты: ("<<numi+1<<"; "<<numj+1<<")\n\n";
+    for(int j=0;j<n;j++)
+        std::swap(arr[0][j],arr[numi][j]);
+    if(numi!=0)
+    {
+        std::cout<<"Строка 1 и строка "<<numi+1<<" поменялись местами\n\n";
+        for(int i=0;i<n;i++)
+        {
+            if(i==0) std::cout<<numi+1;
+            else if(i==numi) std::cout<<'1';
+            else std::cout<<i+1;
+            std::cout<<")\t";
+            for(int j=0;j<n;j++)
+            {
+                std::cout<<arr[i][j]<<'\t';
+            }
+            std::cout<<"\n";
+        }
+    }
+    else std::cout<<"Минимальный элемент уже на первой строке\n";
+}
+

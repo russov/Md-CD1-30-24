51a25cec593392a46826fe4c9f125a28c91e6720
kebetsV <kebets1995@gmail.com>
2024-04-11 15:41:19 +0300

    im still trying to understand how to work with git....

---
+++ Vladislav_Kebets/task3/task3.2/task3.2/task3.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+int main()
+{
+    std::string FileName;
+    std::cout << "Enter file name(example: in.cpp): ";
+    std::cin >> FileName;
+    std::ifstream inf(FileName);
+    if (!inf) {
+        std::cout << "ERROR! FAILED TO OPEN FILE";
+        exit(1);
+    }
+    char sym;
+    int count{0};
+    while (inf >> sym) {
+            if (sym == '{')++count;
+            else if (sym == '}')--count;
+    }
+    std::ofstream outf("out.txt");
+    if (count == 0) {
+        std::cout << "File is ok";
+        outf << "File is ok";
+    }
+    else {
+        std::cout << "File is NOT ok";
+        outf << "File is NOT ok";
+    }
+}

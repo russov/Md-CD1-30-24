7ce3334326dd8e096f46dfba346446cd6a6d3d9b
Vlad <wladik2721248@ya.ru>
2024-04-13 10:01:13 +0300

    Salnikov_Task3

---
+++ Vlad_Salnikov/Task3/first_point_of_the_task/point1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <random>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int array[10][10];
+	int a;
+	int b;
+
+	std::cout << "Введите интервал чисел: ";
+	std::cin >> a >> b;
+
+	std::random_device random_device;
+	std::mt19937 generator(random_device());
+	std::uniform_int_distribution<> distribution(a, b);
+
+	for (int i = 0; i < 10; i++)
+		for (int k = 0; k < 10; k++)
+		{
+			array[i][k] = distribution(generator);
+		}
+
+	int numMIN{array[0][0]};
+	int str{0};
+	for (int i = 0; i < 10; i++)
+	{
+		for (int k = 0; k < 10; k++)
+		{
+			if (array[i][k] < numMIN)
+			{
+				str = i;
+				numMIN = array[i][k];
+			}
+		}
+	}
+	
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << '\n';
+		for (int k = 0; k < 10; k++)
+		{
+			std::cout << array[i][k] << ' ';
+
+		}
+	}
+
+	std::cout << std::endl;
+
+	if (str !=0)
+	{
+		for (int i = 0; i < 10; ++i) 
+		{
+			int num;
+			num = array[0][i];
+			array[0][i] = array[str][i];
+			array[str][i] = num;
+		}
+		
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << '\n';
+		for (int k = 0; k < 10; k++)
+		{
+			std::cout << array[i][k] << ' ';
+			
+		}
+	}
+
+	return 0;
+}
---
+++ Vlad_Salnikov/Task3/second_point_of_the_task/point2.cpp
@@ -0,0 +1,43 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int counter{ 0 };
+	std::string result;
+	std::string str;
+	std::ifstream file;
+	file.open("test.txt");
+	while (!file.eof())
+	{
+		getline(file, str);
+		for (int i = 0; i < str.size(); i++)
+		{
+			if (str[i] == '{' || str[i] == '}')
+			{
+				++counter;
+			}
+		}
+	}
+
+	if (counter % 2) 
+	{
+		result = "Не все скобки закрыты!";
+		std::cout << result;
+	}
+	else
+	{
+		result = "Все скобки закрыты!";
+		std::cout << result;
+	}
+
+	std::ofstream fileOUT;
+	fileOUT.open("out.txt", std::ios::out);
+	fileOUT << result << std::endl;
+	fileOUT.close();
+
+	return 0;
+}

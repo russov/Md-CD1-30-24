7ee701d7fb9c8ffa7bd4aef239c5d8b92b18028f
cherrySunset <a.kartavitsky@gmail.com>
2024-04-17 16:46:16 +0300

    add gitignore and close opened files

--- Alexandr_Kartavitsky/task3/check_brackets.h
+++ Alexandr_Kartavitsky/task3/check_brackets.h
@@ -6,26 +6,35 @@
 #include <stack>
 #include <string>
 
-std::string checkBrackets(const std::string& filename) {
+std::string checkBrackets(const std::string &filename)
+{
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         return "Unable to open file: " + filename;
     }
 
     std::ofstream resultFile("result.txt");
-    if (!resultFile.is_open()) {
+    if (!resultFile.is_open())
+    {
         return "Unable to open result file";
     }
 
     std::stack<char> brackets;
 
     std::string line;
-    while (std::getline(file, line)) {
-        for (char c : line) {
-            if (c == '{') {
+    while (std::getline(file, line))
+    {
+        for (char c : line)
+        {
+            if (c == '{')
+            {
                 brackets.push(c);
-            } else if (c == '}') {
-                if (brackets.empty() || brackets.top() != '{') {
+            }
+            else if (c == '}')
+            {
+                if (brackets.empty() || brackets.top() != '{')
+                {
                     resultFile << "There is a discrepancy\n";
                     return "There is a discrepancy";
                 }
@@ -34,13 +43,17 @@ std::string checkBrackets(const std::string& filename) {
         }
     }
 
-    if (!brackets.empty()) {
+    if (!brackets.empty())
+    {
         resultFile << "There is a discrepancy\n";
         return "There is a discrepancy";
     }
 
     resultFile << "All brackets are in place\n";
     return "All brackets are in place";
+
+    file.close();
+    resultFile.close();
 }
 
 #endif

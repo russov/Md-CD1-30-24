1d2b457dc2180c79762bf09993df2cc0e7cf0741
C0rnFl0wer <164394522+C0rnFl0wer@users.noreply.github.com>
2024-04-21 13:31:38 +0300

    Update Source.cpp

--- Vasilij_Kluev/Task_2_Kluev/Source.cpp
+++ Vasilij_Kluev/Task_2_Kluev/Source.cpp
@@ -12,30 +12,30 @@ int main()
     setlocale(LC_ALL, "ru");
     int size;
     int size2 = 0;
-    cout << "Введите количество чисел, которое хотите записать" << endl;
+    cout << "Enter how many number you wont to enter" << endl;
     while (!(cin >> size))
     {
         cin.clear();
         while (cin.get() != '\n');
-        cout << "Неверный ввод. Повторите." << endl;
-        cout << "Введите число " << endl;
+        cout << "Incorrect. Plise try again." << endl;
+        cout << "Enter number " << endl;
     }
     size = fabs(size);
     int* Arr = new int[size];
     for (int i = 0; i < size; ++i)
     {
         int buffer;
-        cout << "Введите число " << endl;
+        cout << "Enter number " << endl;
         while (!(cin >> buffer))
         {
             cin.clear();
             while (cin.get() != '\n');
-            cout << "Неверный ввод. Повторите." << endl;
-            cout << "Введите число " << endl;
+            cout << "Incorrect. Plise try again." << endl;
+            cout << "Enter number" << endl;
         }
         Arr[i] = buffer;
     }
-    cout << "Проверяем запись вашего массива" << endl;
+    cout << "Check your array" << endl;
     for (int i = 0; i < size; ++i)
     {
         cout << Arr[i] << endl;
@@ -46,7 +46,7 @@ int main()
     Lesson_3.open(path);
     if (!Lesson_3.is_open())
     {
-        cout << "Ошибка!";
+        cout << "Error!";
     }
     else
     {
@@ -65,7 +65,7 @@ int main()
     int* NewArr = new int[size];
     if (!Lesson_3_read.is_open())
     {
-        cout << "Ошибка!" << endl;
+        cout << "Error!" << endl;
     }
     else
     {
@@ -88,7 +88,7 @@ int main()
     Lesson_3_read.close();
     delete[] NewArr;
 
-    cout << "Новый массив!" << endl;
+    cout << "Your new array!" << endl;
 
 
     ifstream Lesson_3_SUM;
@@ -98,7 +98,7 @@ int main()
     int sum = 0;
     if (!Lesson_3_SUM.is_open())
     {
-        cout << "Ошибка" << endl;
+        cout << "Error" << endl;
     }
     else
     {
@@ -109,7 +109,7 @@ int main()
             sum = sum + Arr_SUM[i];
         }
         Lesson_3_SUM.close();
-        cout << "Сумма чисел введенных вами равна:" << sum << endl;
+        cout << "Sum is:" << sum << endl;
     }
     delete[] Arr_SUM;
 
@@ -123,7 +123,7 @@ int main()
     string b;
     if (!Lesson_3_arithmetic.is_open())
     {
-        cout << "Ошибка" << endl;
+        cout << "Error" << endl;
     }
     else
     {
@@ -134,18 +134,18 @@ int main()
             sum1 = sum1 + Arr_arithmetic[i];
         }
         arithmetic = sum1 / size;
-        cout << "Среднеарифмитическое равно:" << arithmetic << endl;
+        cout << "Arithmetic mean" << arithmetic << endl;
     }
     delete[] Arr_arithmetic;
     ofstream Lesson_3_New_number;
     Lesson_3_New_number.open(path, ofstream::app);
     if (!Lesson_3_New_number.is_open())
     {
-        cout << "Ошибка" << endl;
+        cout << "Error" << endl;
     }
     else
     {
-        cout << "Xотите добавить еще одно число?" << endl;
+        cout << "Do you whant to enter new number?" << endl;
         cout << "y/n" << endl;
         string resalt;
         cin >> resalt;
@@ -154,18 +154,18 @@ int main()
         {
             while (is_quel)
             {
-                cout << "Введите число" << endl;
+                cout << "Enter the number" << endl;
                 int new_value;
                 while (!(cin >> new_value))
                 {
                     cin.clear();
                     while (cin.get() != '\n');
-                    cout << "Неверный ввод. Повторите." << endl;
-                    cout << "Введите число " << endl;
+                    cout << "Incorrect. Plise try again." << endl;
+                    cout << "Enter the number " << endl;
                 }
                 ++size2;
                 Lesson_3_New_number << new_value << "\n";
-                cout << "Xотите добавить еще одно число?" << endl;
+                cout << "Do you whant to enter new number?" << endl;
                 cout << "y/n" << endl;
                 cin >> resalt;
                 if (resalt == "Y" || resalt == "y")
@@ -175,7 +175,7 @@ int main()
                 else
                 {
                     is_quel = false;
-                    cout << "конец программы" << endl;
+                    cout << "The End" << endl;
                 }
             }
 
@@ -183,18 +183,18 @@ int main()
         }
         else if (resalt == "N" || resalt == "n")
         {
-            cout << "конец программы" << endl;
+            cout << "The End" << endl;
 
         }
         else
         {
-            cout << "Вы ввели не коректное значение" << endl;
+            cout << "Incorrect." << endl;
         }
         Lesson_3_New_number.close();
     }
 
 
-    cout << "Вычсляем  сумму и среднее значение из нового списка;" << endl;
+    cout << "Count SUM and arithmetic mean at new array:" << endl;
 
     ifstream new_read;
     new_read.open(path);
@@ -204,7 +204,7 @@ int main()
     string new_number_read;
     if (!new_read.is_open())
     {
-        cout << "ОШИБКА!" << endl;
+        cout << "Error!" << endl;
     }
     else
     {
@@ -215,10 +215,10 @@ int main()
             newSUM += New_Arrr_of_Numbers[i];
 
         }
-        cout << "новая сумма равна:" << newSUM << endl;
+        cout << "New SUM:" << newSUM << endl;
         float newSize2 = newSize;
         newArithmetic = newSUM / newSize2;
-        cout << "новое среднеарифметическое:" << newArithmetic << endl;
+        cout << "New arithmetic mean:" << newArithmetic << endl;
     }
     delete[] New_Arrr_of_Numbers;
     new_read.close();

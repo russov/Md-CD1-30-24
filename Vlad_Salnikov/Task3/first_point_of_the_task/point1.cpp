53e4e46cd4e476a49a12aac2015f5899126b623e
Vlad <wladik2721248@ya.ru>
2024-04-24 00:03:44 +0300

    fix magic number

--- Vlad_Salnikov/Task3/first_point_of_the_task/point1.cpp
+++ Vlad_Salnikov/Task3/first_point_of_the_task/point1.cpp
@@ -3,7 +3,8 @@
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	int array[10][10];
+        const int arrSize{ 10 };
+	int array[arrSize][arrSize];
 	int a{ 0 };
 	int b{ 0 };
 
@@ -14,17 +15,17 @@ int main() {
 	std::mt19937 generator(random_device());
 	std::uniform_int_distribution<> distribution(a, b);
 
-	for (int i = 0; i < 10; i++)
-		for (int k = 0; k < 10; k++)
+	for (int i = 0; i < arrSize; i++)
+		for (int k = 0; k < arrSize; k++)
 		{
 			array[i][k] = distribution(generator);
 		}
 
 	int numMIN{array[0][0]};
 	int str{0};
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < arrSize; i++)
 	{
-		for (int k = 0; k < 10; k++)
+		for (int k = 0; k < arrSize; k++)
 		{
 			if (array[i][k] < numMIN)
 			{
@@ -36,7 +37,7 @@ int main() {
 
 	if (str !=0)
 	{
-		for (int i = 0; i < 10; ++i) 
+		for (int i = 0; i < arrSize; ++i) 
 		{
 			int num = array[0][i];
 			array[0][i] = array[str][i];
@@ -45,10 +46,10 @@ int main() {
 		
 	}
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < arrSize; i++)
 	{
 		std::cout << '\n';
-		for (int k = 0; k < 10; k++)
+		for (int k = 0; k < arrSize; k++)
 		{
 			std::cout << array[i][k] << ' ';
 			

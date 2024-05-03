35ce34b84bd16c71c479b94cfea9c1ec3929018d
waldemurr <66300725+waldemurr@users.noreply.github.com>
2024-05-04 01:34:38 +0300

    added tests for string

--- Vladimir_Shedzko/Lesson 11/task_5.cpp
+++ Vladimir_Shedzko/Lesson 11/task_5.cpp
@@ -1,37 +1,91 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include <string>
 
 TEST(StringTests, testConstructor) {
-    std::string s0 {"Initial string"};
+    std::string s0 {"Initial string"}; // initializer list constructor
+    EXPECT_EQ(s0, "Initial string");
 
     std::string s1; // init with empty string
-    ASSERT_EQ(s1, "");
-    std::string s2 (s0); // init with copy of other string
-    ASSERT_EQ(s2, s0);
-    std::string s3 (s0, 8, 3); // init with substring from pos 8 of size 3
-    ASSERT_EQ(s3, "str");
-    std::string s4 ("A character sequence"); // init with c-string
-    ASSERT_EQ(s4, "A character sequence");
-    std::string s5 ("Another character sequence", 12); // init with first 12 symbols of c-string
-    ASSERT_EQ(s5, "Another char");
-    std::string s6a (10, 'x'); // init with 10 'x'
-    ASSERT_EQ(s6a, "xxxxxxxxxx");
-    std::string s6b (10, 42);      // 42 is the ASCII code for '*'
-    ASSERT_EQ(s6b, "**********");
-    std::string s7 (s0.begin(), s0.begin()+7);
-    ASSERT_EQ(s7, "Initial"); // first 7 characters
+    EXPECT_EQ(s1, "");
 
-    // ASSERT_THROW(std::string s7a (s0.begin(), s0.begin()+70), std::out_of_range);
+    std::string s2 (s0); // init with copy of other string
+    EXPECT_EQ(s2, s0);
+    std::string s2a (s0, 8); // init with substring from pos 8
+    EXPECT_EQ(s2a, "string");
+    std::string s2b (s0, 8, 3); // init with substring from pos 8 of size 3
+    EXPECT_EQ(s2b, "str");
+
+    std::string s3 ("A character sequence"); // init with c-string
+    EXPECT_EQ(s3, "A character sequence");
+    std::string s3a ("A character sequence", 11); // init with first c-string 12 chars
+    EXPECT_EQ(s3a, "A character");
+
+    std::string s4 ("Another character sequence", 12); // init with first 12 symbols of c-string
+    EXPECT_EQ(s4, "Another char");
+
+    std::string s5a (10, 'x'); // init with 10 'x'
+    EXPECT_EQ(s5a, "xxxxxxxxxx");
+    std::string s5b (10, 42);      // 42 is the ASCII code for '*'
+    EXPECT_EQ(s5b, "**********");
+
+    std::string s6a (s0.begin(), s0.begin()+7); // 7 characters from begin pointer
+    EXPECT_EQ(s6a, "Initial"); 
+    std::string s6b (s0.begin(), s0.end()); // copy from begin to end
+    EXPECT_EQ(s6b, "Initial string"); 
+
+    std::string s7 = std::move(s0); // move constructor
+    EXPECT_EQ(s7, "Initial string"); 
+    EXPECT_EQ(s0, ""); 
 }
+
 TEST(StringTests, testPushBack) {
     std::string s0 {"Initial string"};
     s0.push_back('!');
-    ASSERT_EQ(s0, "Initial string!");
+    EXPECT_EQ(s0, "Initial string!");
+}
 
-    std::string s1 {'*'};
-    long long max_bits {64};
-    for (int i=0; i < max_bits; i++)
-        s1+=s1;
+TEST(StringTests, testSize) {
+    std::string s0 {"Initial string of quite big size"};
+    EXPECT_EQ(s0.size(), 32);
+}
+
+TEST(StringTests, testFind) {
+    auto s0 = std::string("test string in C is still a string");
     
-    // ASSERT_THROW(s1.push_back('*'), std::length_error);
+    EXPECT_EQ(s0.find('t'), 0);
+    EXPECT_EQ(s0.find('t', 1), 3);
+
+    EXPECT_EQ(s0.find("string"), 5);
+    EXPECT_EQ(s0.find("string", 15), 28);
+}
+
+TEST(StringTests, testResize) {
+    std::string s0 {"Resize test string in C"};
+    s0.resize(18);
+    EXPECT_EQ(s0, "Resize test string");
+
+    std::string s1a {"Resize test string in C"};
+    s1a.resize(25, '+');
+    EXPECT_EQ(s1a, "Resize test string in C++");
+    std::string s1b {"Resize test string in C"};
+    s1b.resize(25, 112);
+    EXPECT_EQ(s1b, "Resize test string in Cpp");
+}
+
+TEST(StringTests, testPopBack) {
+    auto s0 = std::string("test string in C++");
+    
+    s0.pop_back();
+    s0.pop_back();
+    EXPECT_EQ(s0, "test string in C");
+}
+
+TEST(StringTests, testSwap) {
+    auto s0 = std::string("test string in C++");
+    auto s1 = std::string("kinda other string");
+
+    s0.swap(s1);
+
+    EXPECT_EQ(s0, "kinda other string");
+    EXPECT_EQ(s1, "test string in C++");
 }

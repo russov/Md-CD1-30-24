5e5fdc2c8af3746863413de583cea456e246c921
kebetsV <kebets1995@gmail.com>
2024-05-15 21:28:53 +0300

    add MyString

---
+++ Vladislav_Kebets/task_5/MyString/MyString.cpp
@@ -0,0 +1,228 @@
+﻿#include <iostream>
+#include <cString>
+namespace MyString
+{
+    int npos = -1;
+    typedef unsigned int uint;
+    class String
+    {
+    private:
+        char* m_text = nullptr;
+        uint m_len = 0;
+    public:
+
+        String() //EmptyStringConstructor
+        {
+        }
+
+        String(const char* text) //FromCString
+        {
+            m_len = strlen(text);
+            m_text = new char[m_len + 1];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                this->m_text[i] = text[i];
+            }
+        }
+
+        String(const char* text, uint size) //FromSequence
+        {
+            if (size > strlen(text))
+                throw std::exception();
+            m_len = size;
+            m_text = new char[m_len + 1];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                this->m_text[i] = text[i];
+            }
+
+        }
+
+        String(const std::initializer_list<char>& list) //InitializerList
+        {
+            m_len = std::size(list);
+            m_text = new char[m_len + 1];
+            int index = 0;
+            for (auto a : list)
+            {
+                m_text[index] = a;
+                ++index;
+            }
+        }
+
+        String(const String& str, const uint start, uint size) //SubStringConstructor
+        {
+            if (start + size > str.m_len)
+            {
+                this->m_len = str.m_len - start;
+            }
+            else
+            {
+                this->m_len = size;
+            }
+            this->m_text = new char[m_len];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                this->m_text[i] = str.m_text[start + i];
+            }
+        }
+
+        String(int size, char fill) //FillConstructor
+        {
+            m_len = size;
+            m_text = new char[m_len];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                m_text[i] = fill;
+            }
+        }
+
+        String(const String& str) //CopyConstructor
+        {
+            this->m_len = str.m_len;
+            this->m_text = new char[m_len + 1];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                this->m_text[i] = str.m_text[i];
+            }
+        }
+
+        String(String&& move) noexcept//MoveConstructor
+        {
+            m_len = move.m_len;
+            this->m_text = new char[m_len];
+            for (uint i = 0; i < this->m_len; ++i)
+            {
+                this->m_text[i] = move.m_text[i];
+            }
+            move.m_text = nullptr;
+            move.m_len = 0;
+        }
+
+        ~String()
+        {
+            delete[] this->m_text;
+        }
+
+        bool operator== (const char* text) const
+        {
+            if (m_len != strlen(text))
+            {
+                return false;
+            }
+            for (uint i = 0; i < m_len; ++i)
+            {
+                if (m_text[i] != text[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool operator== (const String str) const
+        {
+            if (this->m_len != str.m_len)
+            {
+                return false;
+            }
+            for (uint i = 0; i < m_len; ++i)
+            {
+                if (m_text[i] != str.m_text[i])
+                    return false;
+            }
+            return true;
+        }
+
+        void push_back(const char& sym)
+        {
+            char* text = m_text;
+            ++m_len;
+            m_text = new char[m_len + 1];
+            for (uint i = 0; i < m_len - 1; ++i)
+            {
+                m_text[i] = text[i];
+            }
+            m_text[m_len - 1] = sym;
+        }
+
+        int find(const char& sym, const uint& start = 0)
+        {
+            for (uint i = start; i < m_len; ++i)
+            {
+                if (m_text[i] == sym) return i;
+            }
+            return npos;
+        }
+
+        int find(const String& str, const uint& start = 0)
+        {
+            int found{ 0 };
+            for (uint i = start; i < m_len; ++i)
+            {
+                if (m_text[i] == str.m_text[0])
+                {
+                    found = 1;
+                    for (uint j = i + 1; j < m_len; ++j)
+                    {
+                        if (m_text[j] == str.m_text[found])
+                        {
+                            ++found;
+                            if (found == str.m_len)
+                                return i;
+                        }
+                    }
+                }
+            }
+            return npos;
+        }
+
+        uint size()
+        {
+            return m_len;
+        }
+
+        char* getText()
+        {
+            return m_text;
+        }
+
+        char& front()
+        {
+            return m_text[0];
+        }
+
+        void erase(const uint& start, uint er_size)
+        {
+            if (start > m_len)
+                throw std::exception();
+            if (start + er_size > m_len)
+                er_size = m_len - start;
+            char* buff = new char[m_len];
+            for (uint i = 0; i < m_len; ++i)
+            {
+                buff[i] = this->m_text[i];
+            }
+            delete[] m_text;
+            m_len -= er_size;
+            m_text = new char[m_len + 1];
+            for (uint i = 0; i < start; ++i)
+            {
+                m_text[i] = buff[i];
+            }
+            for (uint i = start + er_size; i < m_len + er_size; ++i)
+            {
+                m_text[i - er_size] = buff[i];
+            }
+            delete[] buff;
+        }
+
+        void print()
+        {
+            for (int i = 0; i < m_len; ++i)
+            {
+                std::cout << m_text[i];
+            }
+        }
+    };
+}
--- Vladislav_Kebets/task_5/task_5/test.cpp
+++ Vladislav_Kebets/task_5/task_5/test.cpp
@@ -1,93 +1,85 @@
 #include "pch.h"
-#include <string>
+#include <String>
+#include "../MyString/MyString.cpp"
 
 TEST(TestConstructor, EmptyStringConstructor) {
-	std::string str;
+	MyString::String str;
 	EXPECT_EQ(str, "");
 }
 
 TEST(TestConstructor, InitializerList) {
 	std::initializer_list<char> list{ 's','t','r','i','n','g' };
-	std::string str(list);
-	EXPECT_EQ(str,"string");
+	MyString::String str(list);
+	EXPECT_EQ(str, "string");
 }
 
 TEST(TestConstructor, CopyConstructor) {
-	std::string str{"string"};
-	std::string str2(str);
-	EXPECT_EQ(str2, "string");
+	MyString::String str{ "String" };
+	MyString::String str2(str);
+	EXPECT_EQ(str2, "String");
 }
 
-TEST(TestConstructor, SubstringConstructor) {
-	std::string str{ "string" };
-	std::string str2(str,2,3);
+TEST(TestConstructor, SubStringConstructor) {
+	MyString::String str{ "String" };
+	MyString::String str2(str, 2, 3);
 	EXPECT_EQ(str2, "rin");
-	std::string str3(str, 2, 100);
+	MyString::String str3(str, 2, 100);
 	EXPECT_EQ(str3, "ring");
 }
 
 TEST(TestConstructor, FromCString) {
-	const char* text{ "string" };
-	std::string str(text);
-	EXPECT_EQ(str, "string");
+	const char* text{ "String" };
+	MyString::String str(text);
+	EXPECT_EQ(str, "String");
 }
 
 TEST(TestConstructor, FromSequence) {
-	const char* text{ "string" };
-	std::string str(text,3);
-	EXPECT_EQ(str, "str");
+	const char* text{ "String" };
+	MyString::String str(text, 3);
+	EXPECT_EQ(str, "Str");
 }
 
 TEST(TestConstructor, FillConstructor) {
-	std::string str(5, 'q');
+	MyString::String str(5, 'q');
 	EXPECT_EQ(str, "qqqqq");
 }
 
 TEST(TestConstructor, MoveConstructor) {
-	std::string str{ "string" };
-	std::string str2(std::move(str));
+	MyString::String str{ "String" };
+	MyString::String str2(std::move(str));
 	EXPECT_EQ(str, "");
-	EXPECT_EQ(str2, "string");
+	EXPECT_EQ(str2, "String");
 }
 
 TEST(TestStringMetod, PushBack) {
-	std::string str{ "somethin" };
+	MyString::String str{ "somethin" };
 	str.push_back('g');
 	EXPECT_EQ(str, "something");
 }
 
 TEST(TestStringMetod, Size) {
-	std::string str{ "something" };
+	MyString::String str{ "something" };
 	EXPECT_EQ(str.size(), 9);
 }
 
 TEST(TestStringMetod, Find) {
-	std::string str{ "something s" };
+	MyString::String str{ "something s" };
 	EXPECT_EQ(str.find('o'), 1);
 	EXPECT_EQ(str.find('s', 1), 10);
-	EXPECT_EQ(str.find('o', 4), std::string::npos);
+	EXPECT_EQ(str.find('o', 4), MyString::npos);
 	EXPECT_EQ(str.find("meth"), 2);
 }
 
-TEST(TestStringMetod, Swap) {
-	std::string str{ "something" };
-	std::string str2{ "nothing" };
-	swap(str, str2);
-	EXPECT_EQ(str, "nothing");
-	EXPECT_EQ(str2, "something");
-
-}
-
 TEST(TestStringMetod, Front) {
-	std::string str{ "something" };
+	MyString::String str{ "something" };
 	EXPECT_EQ(str.front(), 's');
-	str.front()='S';
+	str.front() = 'S';
 	EXPECT_EQ(str.front(), 'S');
 	EXPECT_EQ(str, "Something");
 }
 
 TEST(TestStringMetod, Erase) {
-	std::string str{ "something" };
+	MyString::String str{ "something" };
 	str.erase(6, 1);
 	EXPECT_EQ(str, "somethng");
 	str.erase(4, 100);

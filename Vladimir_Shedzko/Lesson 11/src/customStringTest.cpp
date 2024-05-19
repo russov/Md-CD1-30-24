8884b3f14bb5460013ce63009cfb235b78bd87ee
waldemurr <66300725+waldemurr@users.noreply.github.com>
2024-05-20 00:37:22 +0300

    полностью функционирующая собственная строка

--- Vladimir_Shedzko/Lesson 11/src/customStringTest.cpp
+++ Vladimir_Shedzko/Lesson 11/src/customStringTest.cpp
@@ -2,70 +2,69 @@
 
 #include "customString.h"
 
-TEST(StringConstructor, list) {
+TEST(CustomStringConstructor, list) {
     CustomString s0 {"Initial string"}; // initializer list constructor
     EXPECT_EQ(s0, "Initial string");
 }
 
-TEST(StringConstructor, empty) {
+TEST(CustomStringConstructor, empty) {
     CustomString s1; // init with empty string
     EXPECT_EQ(s1, "");
 }
 
-TEST(StringConstructor, copy) {
+TEST(CustomStringConstructor, copy) {
     CustomString s0 {"Initial string"};
     CustomString s2 (s0); // init with copy of other string
     EXPECT_EQ(s2, s0);
 }
 
-TEST(StringConstructor, copyFromPos) {
+TEST(CustomStringConstructor, copyFromPos) {
     CustomString s0 {"Initial string"};
     CustomString s2a (s0, 8); // init with substring from pos 8
     EXPECT_EQ(s2a, "string");
 }
 
-TEST(StringConstructor, copyFromPosWithCount) {
+TEST(CustomStringConstructor, copyFromPosWithCount) {
     CustomString s0 {"Initial string"};  // initializer list constructor
 
     CustomString s2b (s0, 8, 3); // init with substring from pos 8 and count 3
     EXPECT_EQ(s2b, "str");
 }
 
-TEST(StringConstructor, cString) {
+TEST(CustomStringConstructor, cString) {
     CustomString s3 ("A character sequence"); // init with c-string
     EXPECT_EQ(s3, "A character sequence");
 }
 
-TEST(StringConstructor, cStringWithLimit) {
+TEST(CustomStringConstructor, cStringWithLimit) {
     CustomString s3a ("A character sequence", 11); // init with first c-string 11 chars
     EXPECT_EQ(s3a, "A character");
 }
 
-TEST(StringConstructor, charRange) {
+TEST(CustomStringConstructor, charRange) {
     CustomString s4a (10, 'x'); // init with 10 'x'
     EXPECT_EQ(s4a, "xxxxxxxxxx");
 }
 
-TEST(StringConstructor, charCodeRange) {
+TEST(CustomStringConstructor, charCodeRange) {
     CustomString s4b (10, 42);      // 42 is the ASCII code for '*'
     EXPECT_EQ(s4b, "**********");
 }
 
-TEST(StringConstructor, beginWithSize) {
+TEST(CustomStringConstructor, beginWithSize) {
     CustomString s0 {"Initial string"}; // initializer list constructor
-
     CustomString s5a (s0.begin(), s0.begin() + 7); // 7 characters from begin pointer
     EXPECT_EQ(s5a, "Initial"); 
 }
 
-TEST(StringConstructor, beginToEnd) {
+TEST(CustomStringConstructor, beginToEnd) {
     CustomString s0 {"Initial string"}; // initializer list constructor
 
-    CustomString s5b (s0.begin(), s0.end()); // 3 characters before end pointer
+    CustomString s5b (s0.begin(), s0.end()); // from begin to end
     EXPECT_EQ(s5b, "Initial string"); 
 }
 
-TEST(StringConstructor, move) {
+TEST(CustomStringConstructor, move) {
     CustomString s0 {"Initial string"}; // initializer list constructor
 
     CustomString s6 = std::move(s0); // move constructor
@@ -73,72 +72,72 @@ TEST(StringConstructor, move) {
     EXPECT_EQ(s0, ""); 
 }
 
-TEST(StringPushBack, testPushBack) {
+TEST(CustomStringPushBack, testPushBack) {
     CustomString s0 {"Initial string"};
     s0.push_back('!');
     EXPECT_EQ(s0, "Initial string!");
 }
 
-TEST(StringSize, testSize) {
+TEST(CustomStringSize, testSize) {
     CustomString s0 {"Initial string of quite big size"};
     EXPECT_EQ(s0.size(), 32);
 }
 
-TEST(StringFind, testFindChar) {
+TEST(CustomStringFind, testFindChar) {
     auto s0 = CustomString("test string in C is still a string");
     
     EXPECT_EQ(s0.find('t'), 0); // find a character
 }
 
-TEST(StringFind, testFindCharFromPos) {
+TEST(CustomStringFind, testFindCharFromPos) {
     auto s0 = CustomString("test string in C is still a string");
     
     EXPECT_EQ(s0.find('t', 1), 3); // find a character from pos
 }
 
-TEST(StringFind, testFindNonExistentChar) {
+TEST(CustomStringFind, testFindNonExistentChar) {
     auto s0 = CustomString("test string in C is still a string");
     
     EXPECT_EQ(s0.find('z'), CustomString::npos); // find a non-existent character
 }
 
-TEST(StringFind, testFindString) {
+TEST(CustomStringFind, testFindString) {
         auto s0 = CustomString("test string in C is still a string");
 
     EXPECT_EQ(s0.find("string"), 5); // find a const string
 }
 
-TEST(StringFind, testFindStringFromPos) {
+TEST(CustomStringFind, testFindStringFromPos) {
     auto s0 = CustomString("test string in C is still a string");
 
     EXPECT_EQ(s0.find("string", 15), 28); // find a const string from pos
 }
 
-TEST(StringFind, testFindNonExistentString) {
+TEST(CustomStringFind, testFindNonExistentString) {
     auto s0 = CustomString("test string in C is still a string");
 
     EXPECT_EQ(s0.find("strng"), CustomString::npos); // find a non-existent const string
 }
 
-TEST(StringResize, resizeLess) {
+TEST(CustomStringResize, resizeLess) {
     CustomString s0 {"Resize test string in C"};
     s0.resize(18);
     EXPECT_EQ(s0, "Resize test string");
 }
 
-TEST(StringResize, resizeWithChar) {
+TEST(CustomStringResize, resizeWithChar) {
     CustomString s1a {"Resize test string in C"}; // resize with defined const char
     s1a.resize(25, '+');
     EXPECT_EQ(s1a, "Resize test string in C++");
 }
 
-TEST(StringResize, resizeWithCharCode) {
+TEST(CustomStringResize, resizeWithCharCode) {
     CustomString s1b {"Resize test string in C"}; // resize with char code
     s1b.resize(25, 112);
     EXPECT_EQ(s1b, "Resize test string in Cpp");
 }
 
-TEST(StringPopBack, testPopBack) {
+TEST(CustomStringPopBack, testPopBack) {
     auto s0 = CustomString("test string in C++");
     
     s0.pop_back();
@@ -147,7 +146,7 @@ TEST(StringPopBack, testPopBack) {
     EXPECT_EQ(s0, "test string in C");
 }
 
-TEST(StringSwap, testSwap) {
+TEST(CustomStringSwap, testSwap) {
     auto s0 = CustomString("test string in C++");
     auto s1 = CustomString("kinda other string");
 

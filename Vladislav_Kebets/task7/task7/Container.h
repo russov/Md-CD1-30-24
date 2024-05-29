7b6b69d80fd7fb5f96ad37d30643954943f324fe
kebetsV <kebets1995@gmail.com>
2024-05-29 11:02:27 +0300

    add task7

---
+++ Vladislav_Kebets/task7/task7/Container.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+template <class T>
+class Container
+{
+private:
+	std::vector<T> figures;
+public:
+	Container()
+	{}
+
+	void push_back(const T& figure)
+	{
+		this->figures.push_back(figure);
+	}
+
+	void pop_back()
+	{
+		this->figures.pop_back();
+	}
+
+	int size() 
+	{
+		return figures.size();
+	}
+
+	const T operator[](const unsigned int& index) const
+	{
+		return figures.at(index);
+	}
+
+	T operator[](unsigned int& index) const
+	{
+		return figures.at(index);
+	}
+};
+
---
+++ Vladislav_Kebets/task7/task7/Source.cpp
@@ -0,0 +1,10 @@
+#include "../../task7/task7/Container.h"
+#include "../../task7/task7/Figures.h"
+int main()
+{
+	Container<Figure*> container;
+	container.push_back(new Sphere(3));
+	container.push_back(new Cylinder(5, 7));
+	container.push_back(new Pyramid(9, 2));
+	std::cout << container[1]->getInfo();
+}
